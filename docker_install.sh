7854758298cb1740bed8caddc101db07e204b4a9
Gokula Krishnan.K <65504920+krishnansai@users.noreply.github.com>
2022-07-27 13:41:46 +0530

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -2,18 +2,18 @@
 
 
 echo "upgrade total system!..."
-sudo apt upgrade
+sudo apt upgrade -y
 echo "system update!..."
-sudo apt update
+sudo apt update -y
 echo "Installing Docker Engine on Ubuntu!..."
 echo "Uninstalling old versions!.."
 sudo apt-get remove docker docker-engine docker.io containerd runc
 echo "Install using the repository!..."
 echo "Set up the repository!..."
 echo "Your System is updating!...."
-sudo apt-get update
+sudo apt-get update -y
 echo "Install packages to allow apt to use a repository over HTTPS"
-sudo apt-get install \
+sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -27,14 +27,14 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 echo "Installling Docker Engine"
 echo "Your System is updating!...."
-sudo apt-get update
+sudo apt-get update -y
 echo "Installing the latest version of Docker Engine and containerd!..."
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 echo "To install a specific version of Docker Engine!..."
 echo "List the versions available in your repo!..."
 sudo apt-cache madison docker-ce
 echo "Install a specific version!..."
-sudo apt-get install docker-ce=5:20.10.6~3-0~ubuntu-focal docker-ce-cli=5:20.10.6~3-0~ubuntu-focal containerd.io
+sudo apt-get install -y docker-ce=5:20.10.6~3-0~ubuntu-focal docker-ce-cli=5:20.10.6~3-0~ubuntu-focal containerd.io
 echo "Check Docker Engine is installed correctly by running the hello-world image!.."
 sudo docker run docker/whalesay cowsay Docker!
 echo "Docker is Successfull Installed!..."
